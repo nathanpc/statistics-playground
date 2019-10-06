10c3c8b06706a3257476460fc2d9dfde8da6ce0b
Nathan Campos <nathan@innoveworkshop.com>
2019-10-06 15:49:59 +0100

    package-wait - Generating graphs already

--- projects/package-wait/analyze.R
+++ projects/package-wait/analyze.R
@@ -4,6 +4,7 @@
 #' @author Nathan Campos <nathanpc@dreamintech.net>
 
 library(RSQLite)
+library(ggplot2)
 
 #' Retrieves all the carrier names from the database.
 #'
@@ -25,21 +26,45 @@ get_countries <- function (conn) {
     return(unique(countries$origin))
 }
 
+#' Grabs a perfect data frame for plotting the data for a country.
+#'
+#' @param  conn    Database connection.
+#' @param  country Country name.
+#' @return         Data frame to work with the data.
+country_data <- function (conn, country) {
+    data <- dbGetQuery(conn, "SELECT origin, carrier, wait_period, shipped,
+                       delivered FROM Packages WHERE origin = ? ORDER BY
+                       carrier ASC", params = list(country))
+
+    return(data)
+}
+
 #' The script main entry point.
 main <- function () {
     # Connect to database.
     conn <- dbConnect(RSQLite::SQLite(), "data/database.db")
 
-    # Get carriers.
+    # Grab various data points.
     carriers <- get_carriers(conn)
-    print(carriers)
-
-    # Get countries.
     countries <- get_countries(conn)
-    print(countries)
 
-    # Get all the packages.
-    packs <- dbGetQuery(conn, "SELECT * FROM Packages ORDER BY wait_period ASC")
+    # Get the data by country.
+    df <- country_data(conn, "China")
+    print(df)
+
+    # Plot the data.
+    graph <- ggplot(df, aes(x = wait_period, color = carrier, fill = carrier))
+    graph <- graph + geom_density(alpha = 0.5)
+    graph <- graph + labs(title = "Package Wait Period from China",
+                         subtitle = "How long do you have to wait for a package to arrive?",
+                         x = "Wait Period (Days)",
+                         y = "Density",
+                         fill = "Carriers",
+                         color = "Carriers",
+                         caption = "Data and Visualization by Nathan Campos (@nathanpc)")
+    graph <- graph + theme(legend.position = "bottom")
+    graph <- graph + expand_limits(x = 0, y = 0)
+    print(graph)
 
     # Clean up.
     dbDisconnect(conn)
